eb131e861538547c5f47fb9c332a02c2639e322b
bondyuu <leo4902@gmail.com>
2022-09-02 14:05:53 +0900

    fix test controller

--- dodam/src/main/java/com/team1/dodam/controller/TestController.java
+++ dodam/src/main/java/com/team1/dodam/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.team1.dodam.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class TestController {
 

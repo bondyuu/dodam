7d305dc592d1620a13a37a3efc643f73a3ce8d61
bondyuu <leo4902@gmail.com>
2022-09-05 19:56:08 +0900

    [#24]Add: 마이페이지 조회 기능 추가

--- dodam/src/main/java/com/team1/dodam/repository/PostRepository.java
+++ dodam/src/main/java/com/team1/dodam/repository/PostRepository.java
@@ -1,7 +1,10 @@
 package com.team1.dodam.repository;
 
 import com.team1.dodam.domain.Post;
+import com.team1.dodam.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Long countAllByUser(User user);
 }

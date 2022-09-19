7451c03e2f83ec949a41f8739f906d1297aeb3cf
bondyuu <leo4902@gmail.com>
2022-09-19 14:51:35 +0900

    [#62]Fix : accesstoken, refreshtoken 만료 기간 연장

--- dodam/src/main/java/com/team1/dodam/jwt/TokenProvider.java
+++ dodam/src/main/java/com/team1/dodam/jwt/TokenProvider.java
@@ -29,8 +29,8 @@ public class TokenProvider {
 
   private static final String AUTHORITIES_KEY = "auth";
   private static final String BEARER_PREFIX = "Bearer ";
-  private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            //30분
-  private static final long REFRESH_TOKEN_EXPRIRE_TIME = 1000 * 60 * 60 * 24 * 7;     //7일
+  private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 12;            //12시간분
+  private static final long REFRESH_TOKEN_EXPRIRE_TIME = 1000 * 60 * 60 * 24 * 15;     //15일
 
   private final Key key;
   private final RefreshTokenRepository refreshTokenRepository;

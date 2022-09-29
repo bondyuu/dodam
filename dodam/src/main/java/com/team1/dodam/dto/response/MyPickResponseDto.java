485adf35043786f26aa9de90d4b4821a196eb529
kwanghunlee <lkhun9311@naver.com>
2022-09-30 01:06:19 +0900

    [#30] Add: Redis Cache 기능(@Cacheable)을 활용해 내가 찜한 게시글 목록 조회 기능 업그레이드(Cache Aside Pattern)

--- dodam/src/main/java/com/team1/dodam/dto/response/MyPickResponseDto.java
+++ dodam/src/main/java/com/team1/dodam/dto/response/MyPickResponseDto.java
@@ -7,12 +7,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class MyPickResponseDto {
+public class MyPickResponseDto implements Serializable {
     private Long id;
     private String title;
     private String content;
